701e49476d99f4b3b33833ff302d449dec05ed21
Your Name <bartosz.kociecki@gmail.com>
2025-06-16 21:54:23 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,20 @@
 package com.company;
 
+import com.company.animal.Cat;
+import com.company.animal.Dog;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Cat cat = new Cat("blue", "dark");
+        System.out.println(cat.color);
+        System.out.println(cat.color2);
+        cat.catVoice();
+
+        Dog dog = new Dog();
+        System.out.println(dog.color);
+        System.out.println(dog.color2);
+        dog.dogVoice();
+
         }
     }
---
+++ src/com/company/animal/Dog.java
@@ -0,0 +1,12 @@
+package com.company.animal;
+
+public class Dog {
+
+    String dog_voice = "bark";
+    public String color = "blue";
+    public String color2 = "dark";
+
+    public void dogVoice(){
+        System.out.println("Voice od cat is: " + dog_voice);
+    }
+}
